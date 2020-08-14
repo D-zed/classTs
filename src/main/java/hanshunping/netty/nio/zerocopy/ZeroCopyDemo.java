1a21b2e983cd9c17ae569911b42ab081c71b45b2
dengzidi <913985415@qq.com>
2020-08-14 09:52:01 +0800

    零拷贝部分代码及原理

--- src/main/java/hanshunping/netty/nio/zerocopy/ZeroCopyDemo.java
+++ src/main/java/hanshunping/netty/nio/zerocopy/ZeroCopyDemo.java
@@ -13,6 +13,9 @@ package hanshunping.netty.nio.zerocopy;
  * 2 linux 2.4版本的sendFile函数就是一个 加强版本的零拷贝了 它可以直接跳过了用户态，也就不需要拷贝到用户缓冲区了
  * 并且也做到了内核缓冲区的数据只有一份
  * dma到内核缓冲区 内核缓冲区直接到协议栈 两次拷贝 并且根本不需要通过用户态所以减少了状态切换的性能消耗
+ *
+ * 这个不错
+ * https://blog.csdn.net/weixin_30402085/article/details/99029292?utm_medium=distribute.pc_relevant.none-task-blog-baidulandingword-7&spm=1001.2101.3001.4242
  * @author ASUS
  */
 public class ZeroCopyDemo {
