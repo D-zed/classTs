d295b159b3d78fd8a4d7ecfb3866141a47870ca1
dengzidi <913985415@qq.com>
2020-08-20 09:53:23 +0800

    string

--- src/main/java/zhouyang/jvm/StringInternDemo.java
+++ src/main/java/zhouyang/jvm/StringInternDemo.java
@@ -13,8 +13,10 @@ public class StringInternDemo {
     public static void main(String[] args) {
         //str2不加是true 加上是false 首先向池子里拿到
        String str2 = "DSEUCalvin";//
+        String str22 = "DSEUCalvin";
        String str3 = "D";//
 
+        System.out.println(str2==str22);
         //这个拼接产生的新对象在堆中
         String str1 = str3+new String("SEU")+ new String("Calvin");
         //intern默认先去池子里寻找 所以此时二者指向的是同一个了
