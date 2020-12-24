70eb93f1121865b62fced809b085d49a993404f4
dengzidi <913985415@qq.com>
2020-12-24 17:25:04 +0800

    使用stream流计算单词个数

---
+++ src/main/java/naixue/two/stream/TestStream.java
@@ -0,0 +1,29 @@
+package naixue.two.stream;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TestStream {
+
+    static void wordCount() {
+        //1. 模拟数据源 两行数据
+        List<String> text = Arrays.asList("hello java", "hello python");
+
+       //2 将数据源碾碎，重新构建一个流
+        Stream<String> stringStream = text.stream().flatMap(w->Stream.of(w.split(" ")));
+        //3 对新的流进行计算
+        Map<String, Long> result = stringStream.collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+        //4 输出结果
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        wordCount();
+    }
+
+
+}
